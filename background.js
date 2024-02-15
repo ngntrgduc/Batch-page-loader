dca619ab31fd65bd619298b8742fbdbe19b31274
ngntrgduc <trungducnguyen1511@gmail.com>
2024-02-15 23:14:29 +0700

    Add shortcut to open options page

--- background.js
+++ background.js
@@ -3,3 +3,12 @@ browser.action.onClicked.addListener(function(tab) {
         url: browser.runtime.getURL("index.html") 
     });
 });
+
+browser.commands.onCommand.addListener((command) => {
+    if (command === "open-options") {
+        browser.tabs.create({ 
+            url: browser.runtime.getURL("options/options.html") 
+        });
+    }
+  });
+  
